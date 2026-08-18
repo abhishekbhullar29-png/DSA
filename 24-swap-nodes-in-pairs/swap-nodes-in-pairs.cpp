14d133f64d5125834ac4d36a37d309ec1fe5602c
abhishekbhullar29-png <abhishekbhullar29@gmail.com>
2026-08-18 22:40:23 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (99.49%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* prev = &dummy;
+
+        while (prev->next != nullptr && prev->next->next != nullptr) {
+            ListNode* first = prev->next;
+            ListNode* second = first->next;
+
+            // Swap the nodes
+            first->next = second->next;
+            second->next = first;
+            prev->next = second;
+
+            // Move to the next pair
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+};
